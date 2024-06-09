26ad4e3d81f79ca1fc22ac21c9623eb67b4bfd10
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-06-09 22:28:57 +0100

    feat: Adding class Ship.

---
+++ src/Ship.java
@@ -0,0 +1,18 @@
+public class Ship {
+
+    private final String name;
+    private final int size;
+
+    public Ship(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
