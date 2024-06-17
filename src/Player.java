d6f1a1e13197a2f1c8af689e27929907a1dfb2a3
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-06-17 19:43:32 +0100

    feat: Adding new behaviour.

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,29 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Player {
-        private final Board board = new Board(10);
+    private final Board board = new Board(10);
+
+
+    public void placeAllShips() {
+        DefaultShip defaultShip = new DefaultShip();
+        List<Ship> defaultShips = defaultShip.initializeDefaultShip();
+
+        Scanner scanner = new Scanner(System.in);
+        for (Ship ship : defaultShips) {
+            boolean placed = false;
+            while (!placed) {
+
+                System.out.println("Placing " + ship.getName() + " of size " + ship.getSize());
+
+
+                System.out.print("Enter starting point like A1 : ");
+                String startingPoint = scanner.nextLine();
+
+                System.out.print("Enter direction (0 for HORIZONTAL, 1 for VERTICAL): ");
+                int directionValue = scanner.nextInt();
+                Direction direction = (directionValue == 0) ? Direction.HORIZONTAL : Direction.VERTICAL;
+            }
+        }
+    }
 }
