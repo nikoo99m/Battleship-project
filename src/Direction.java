1cd6d74eead1f087a3f66e50c14ff18a569cce7a
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-06-18 16:13:15 +0100

    refactor: Removing redundancies.

--- src/Direction.java
+++ src/Direction.java
@@ -1,22 +1,26 @@
 public enum Direction {
-    HORIZONTAL('h', 'H'),
-    VERTICAL('v', 'V');
+    HORIZONTAL,
+   VERTICAL
 
-    private final char lower;
-    private final char upper;
-
-    Direction(char lower, char upper) {
-        this.lower = lower;
-        this.upper = upper;
-    }
-
-    public static Direction decode(char c) {
-        for (Direction direction : Direction.values()) {
-            if (direction.lower == c || direction.upper == c) {
-                return direction;
-            }
-
-        }
-        return null;
-    }}
+}
+//    HORIZONTAL('h', 'H'),
+//    VERTICAL('v', 'V');
+//
+//    private final char lower;
+//    private final char upper;
+//
+//    Direction(char lower, char upper) {
+//        this.lower = lower;
+//        this.upper = upper;
+//    }
+//
+//    public static Direction decode(char c) {
+//        for (Direction direction : Direction.values()) {
+//            if (direction.lower == c || direction.upper == c) {
+//                return direction;
+//            }
+//
+//        }
+//        return null;
+//    }}
 
