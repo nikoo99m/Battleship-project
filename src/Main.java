66786f6b0ab02aba2f1b220a21f062990311f323
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-06-20 21:28:39 +0100

    test: Initialising boards, place ships, and simulate shooting.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args){
 
-        Board board = new Board();
+        Board board = new Board(10);
+        Board enemyboard = new Board(10);
+
         board.printBoard();
+        Player player = new Player(board);
+        Player enemy = new Player(enemyboard);
+        player.placeAllShips();
+        enemy.placeAllShips();
+        player.shoot(enemyboard);
+
     }}
