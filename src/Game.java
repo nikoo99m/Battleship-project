cbe2ddbb6d33a7a5637f3ef414122323c039eaca
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-06-24 19:40:05 +0100

    feat: Adding new behaviour in the Game and starting the game by placing ships.

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,14 @@
 public class Game {
+    private final Player player1;
+    private final Player player2;
+
+    public Game() {
+        player1 = new Player(new Board(10));
+        player2 = new Player(new Board(10));
+    }
+    public void startOfTheGame(){
+        player1.placeAllShips();
+        player2.placeAllShips();
+    }
+
 }
