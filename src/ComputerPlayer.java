69f6edc33274ba298c856d1ddb4c73e76b298d2e
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-06-26 15:24:34 +0100

    refactor: Introducing abstract Player class with HumanPlayer and ComputerPlayer subclasses.

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -1,2 +1,20 @@
-public class ComputerPlayer {
+public class ComputerPlayer extends Player{
+
+    public ComputerPlayer(Board board) {
+        super(board);
+    }
+
+    @Override
+    public void placeAllShips() {
+    }
+
+    @Override
+    public void shoot(Board enemyBoard) {
+
+
+    }
 }
+
+
+
+
--- src/Player.java
+++ src/Player.java
@@ -1,63 +1,11 @@
-import java.util.List;
-import java.util.Scanner;
+public abstract class Player {
 
-public class Player {
-    private Board board = new Board(10);
+        protected Board board;
 
-    Player(Board board) {
-        this.board = board;
-    }
-
-
-    public void placeAllShips() {
-        DefaultShip defaultShip = new DefaultShip();
-        List<Ship> defaultShips = defaultShip.initializeDefaultShip();
-
-        Scanner scanner = new Scanner(System.in);
-
-        for (Ship ship : defaultShips) {
-            boolean placed = false;
-            while (!placed) {
-
-                System.out.println("Placing " + ship.getName() + " of size " + ship.getSize());
-
-                String getRow = "Enter starting row";
-                String getColumn = "Enter starting column";
-                Location location = InputCheck.checkLocationInput(scanner, board, getRow, getColumn);
-                String getDirection = "Enter direction (0 for HORIZONTAL, 1 for VERTICAL): ";
-                Direction direction = InputCheck.checkDirectipnInput(scanner, getDirection);
-
-                ship.setLocation(location);
-                ship.setDirection(direction);
-
-                if (board.addShip(ship)) {
-                    placed = true;
-                    System.out.println(ship.getName() + " placed successfully.");
-                    board.printBoard();
-                } else {
-                    System.out.println("Cannot place ship at specified location. Please try again.");
-
-                }
-            }
+        public Player(Board board) {
+            this.board = board;
         }
+
+        public abstract void placeAllShips();
+        public abstract void shoot(Board enemyBoard);
     }
-
-    public Location shoot(Board enemyBoard) {
-        System.out.println("Enter the row of the point for shoothing");
-        Scanner scanner = new Scanner(System.in);
-        int row = scanner.nextInt();
-        System.out.println("Enter the column of the point for shoothing");
-        Scanner sc = new Scanner(System.in);
-        int column = sc.nextInt();
-        Location firedLocation = new Location(column, row);
-
-        if (enemyBoard.addHit(firedLocation)) {
-            enemyBoard.printBoard();
-            if (enemyBoard.hasHit(firedLocation)) {
-                shoot(enemyBoard);
-            }
-        }
-        return firedLocation;
-    }
-}
-
