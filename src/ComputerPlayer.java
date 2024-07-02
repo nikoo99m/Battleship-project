9c7a4752e589535afce34f49c0a8886113bb2f58
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-07-02 23:00:39 +0100

    feat: Implementing a method for computer player to place ships randomly on the board.

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class ComputerPlayer extends Player{
 
     public ComputerPlayer(Board board) {
@@ -6,7 +8,29 @@ public class ComputerPlayer extends Player{
 
     @Override
     public void placeAllShips() {
+        DefaultShip defaultShip = new DefaultShip();
+        List<Ship> defaultShips = defaultShip.initializeDefaultShip();
+
+        for (Ship ship : defaultShips) {
+            boolean placed = false;
+            while (!placed) {
+                int randomRow = (int) (Math.random() * board.getLength());
+                int randomColumn = (int) (Math.random() * board.getLength());
+                Direction randomDirection = Math.random() < 0.5 ? Direction.HORIZONTAL : Direction.VERTICAL;
+
+                ship.setDirection(randomDirection);
+                ship.setLocation(new Location(randomColumn, randomRow));
+
+                if (board.addShip(ship)) {
+                    placed = true;
+                    System.out.println(ship.getName() + " placed successfully.");
+                    board.printBoard();
+
+                }
+            }
+        }
     }
+
 
     @Override
     public void shoot(Board enemyBoard) {
