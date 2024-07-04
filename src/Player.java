ffeb24cd1e831330ea4199b6bde7fd2931bc867b
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-07-04 20:06:58 +0100

    feat: Adding new behaviours.

--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,25 @@
 public abstract class Player {
+    protected Location lastShot;
+    
+    protected Board board;
 
-        protected Board board;
-
-        public Player(Board board) {
-            this.board = board;
-        }
-
-        public abstract void placeAllShips();
-        public abstract void shoot(Board enemyBoard);
+    public Player(Board board) {
+        this.board = board;
     }
+
+    public abstract void placeAllShips();
+
+    public abstract void shoot(Board enemyBoard);
+
+    public Board getBoard() {
+        return board;
+    }
+    public Location getLastShot() {
+        return lastShot;
+    }
+    protected void setLastShot(Location lastShot) {
+        this.lastShot = lastShot;
+    }
+
+}
+
